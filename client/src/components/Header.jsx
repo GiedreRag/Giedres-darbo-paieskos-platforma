f2f16377e0200822fe3a9381597f5f6dbd4de25d
Giedre <giedre.rageliene@gmail.com>
2023-08-24 15:18:56 +0300

    Footer, Home and Header update.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -11,14 +11,13 @@ export function Header() {
                 </div>
 
                 <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-                    <li><Link to="/" className="nav-link px-2 link-secondary">Home</Link></li>
-                    <li><Link to="/" className="nav-link px-2">Features</Link></li>
-                    <li><Link to="/" className="nav-link px-2">About</Link></li>
+                    <li><Link to="/" className="nav-link px-2 link-secondary">Pagrindinis</Link></li>
+                    <li><Link to="/" className="nav-link px-2">Visi darbo pasiulymai</Link></li>
                 </ul>
 
                 <div className="col-md-3 text-end">
-                    <button type="button" className="btn btn-outline-primary me-2">Login</button>
-                    <button type="button" className="btn btn-primary">Sign-up</button>
+                    <Link to="/" className="btn btn-outline-primary me-2">Prisijunkti</Link>
+                    <Link to="/" className="btn btn-primary">Registruotis</Link>
                 </div>
             </header>
         </div>
