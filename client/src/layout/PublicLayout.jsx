84c21bdc73b27a04a116df5e3f522c82ffb01d2c
Giedre <giedre.rageliene@gmail.com>
2023-08-24 14:10:37 +0300

    Basic header jsx

--- client/src/layout/PublicLayout.jsx
+++ client/src/layout/PublicLayout.jsx
@@ -1,7 +1,9 @@
+import { Header } from '../components/Header';
+
 export function PublicLayout() {
     return (
         <>
-            <>HEADER</>
+            <Header />
             <>CONTENT</>
             <>FOOTER</>
         </>
