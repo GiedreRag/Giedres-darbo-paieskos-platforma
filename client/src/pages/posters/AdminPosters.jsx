5550abaa034b40462465ea54d3105dd79ee38f69
Giedre <giedre.rageliene@gmail.com>
2023-09-09 13:36:32 +0300

    Posters cities and profession filtering

--- client/src/pages/posters/AdminPosters.jsx
+++ client/src/pages/posters/AdminPosters.jsx
@@ -1,12 +1,34 @@
 import { PostersTable } from "../../components/PostersTable";
+import { useContext, useState } from "react";
+import { GlobalContext } from "../../context/GlobalContext";
 
 export function AdminPosters() {
+    const { cities } = useContext(GlobalContext);
+    const [selectedCity, setSelectedCity] = useState('All');
+    const [profession, setProfession] = useState('');
+
     return (
         <div className="container" >
             <h1>Skelbimai</h1>
-
             <div className="col-12">
-                <PostersTable />
+                <div className="row">
+                    <div className="col-6 col-sm-4 col-md-3">
+                        <select className="form-select" 
+                            onChange={e => setSelectedCity(e.target.value)}>
+                            <option value="All">Visi</option>
+                            {cities.map(c => (
+                                <option key={c} value={c}>{c}</option>
+                            ))}
+                        </select>
+                    </div>
+                    <div className="col-6 col-sm-4 col-md-3">
+                        <input type="text" className="form-control" value={profession} 
+                            onChange={e => setProfession(e.target.value)} />
+                    </div>
+                </div>
+            </div>
+            <div className="col-12">
+                <PostersTable filterCity={selectedCity} filterProfession={profession.toLowerCase()}/>
             </div>
         </div>
     );
